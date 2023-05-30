4ba288a806d1397d2eef8cf32c63391e86b38013
LucasDaviid <Lucass_David@outlook.com>
2023-05-30 15:42:43 -0300

    Imagem update para vir do meu github

--- empowerdata_projetos/projeto1/projeto1.py
+++ empowerdata_projetos/projeto1/projeto1.py
@@ -6,7 +6,7 @@ from fpdf import FPDF
 # Dados
 descricao_projeto = input('Digite a descrição do projeto: ')
 horas = input('Digite o total de horas estimadas: ')
-valor = input('Digite o valor da hora trabalhada: ')
+valor = input('Digite o valor da hora trabalhada: R$ ')
 prazo_projeto = input('Digite o prazo estimado: ')
 
 hora_valor = float(valor) 
@@ -18,9 +18,10 @@ total_str = str(total)
 # Gerador de pdf
 pdf = FPDF()
 
-pdf.add_page() # Aqui criamos uma pagina de PDF
+pdf.add_page() # Cria uma pagina em branco no pdf
+pdf.image("https://github.com/LucasDaviid/projetos_python/blob/git@github.com-LucasDaviid/projetos_python.git/empowerdata_projetos/projeto1/template.png?raw=true", x=0, y=0) # Aqui passamos o caminho da imagem para o PDF e as coordenadas para a imagem cobrir a pagina
 pdf.set_font('helvetica', size=14) # Seleção da fonte e tamanho
-pdf.image('rb.png', x=0, y=0) # Aqui passamos a imagem template para o PDF e as coordenadas para a imagem cobrir a pagina
+
 
 # Para escrevermos no pdf totas as variaveis precisam ser str
 pdf.text(115, 145, descricao_projeto)
