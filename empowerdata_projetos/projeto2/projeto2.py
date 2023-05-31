50d266277326b993c32c29013706572bd869300a
LucasDaviid <Lucass_David@outlook.com>
2023-05-31 13:48:05 -0300

    pip e import do yfinance

--- empowerdata_projetos/projeto2/projeto2.py
+++ empowerdata_projetos/projeto2/projeto2.py
@@ -1,2 +1,11 @@
 # Automação de tarefas
+"""
+Passo a passo do problema
+    1 - Buscar dados das ações automaticamente
+    2 - Realizar as análises solicitadas pelo gestor
+    3 - Enviar automaticamente o e-mail com o resultado da análise
+"""
 
+# Passo 1
+
+import yfinance # Extrai dados de ações do Yahoo Finance
